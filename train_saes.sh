1e6ea77781f1be918db69aabf6442447ffadf928
Ahmet Salih Turkel <97659984+ast12233344444@users.noreply.github.com>
2026-02-25 14:44:25 +0300

    added more stuff.

--- train_saes.sh
+++ train_saes.sh
@@ -5,11 +5,5 @@
 #SBATCH --mem-per-gpu=40G
 
 cd /users/ahmet.turkel/CMPE492
-rm -rf .venv
-python -m venv .venv
-source .venv/bin/activate
-export PIP_USER=false
-pip install --upgrade pip
-pip install -r src/requirements.txt
 
 python src/SAE/train_sae.py
