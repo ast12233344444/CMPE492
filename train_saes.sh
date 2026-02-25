7287da81108a9e37ea593bd6d9d242c33761818a
Ahmet Salih Turkel <97659984+ast12233344444@users.noreply.github.com>
2026-02-25 14:41:02 +0300

    added more stuff.

--- train_saes.sh
+++ train_saes.sh
@@ -8,7 +8,7 @@ cd /users/ahmet.turkel/CMPE492
 rm -rf .venv
 python -m venv .venv
 source .venv/bin/activate
-PIP_USER=true
+export PIP_USER=false
 pip install --upgrade pip
 pip install -r src/requirements.txt
 
